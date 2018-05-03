8de6031bcb69de16347f684116c9fcc28dbb622c
inn0vative1 <john.warmann@gmail.com>
2018-05-03 01:23:03 -0400

    Increased gas limit for all networks

--- truffle.js
+++ truffle.js
@@ -6,25 +6,25 @@ module.exports = {
                host: "localhost",
                port: 7545,
                network_id: "*", // Match any network id
-               gas: 4700000
+               gas: 5100000
           },
           private: {
             host: "localhost",
             port: 8545,
             network_id: "4224",
-            gas: 4700000
+            gas: 5100000
           },
           rinkeby: {
             host: "localhost",
             port: 8545,
             network_id: "4", //rinkeby test network
-            gas: 4700000
+            gas: 5100000
           },
           live: {
             host: "localhost",
             port: 8545,
             network_id: "1",
-            gas: 4700000,
+            gas: 5100000,
             gasPrice: 57000000000,
             from: "0xbf6f5e78e154799c7856f4cf9f269cf7a7201310"
           }
