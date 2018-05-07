bd5e886b33fdb1f550a6b427ba3792da249c06b0
John Warmann <john.warmann@gmail.com>
2018-05-06 23:52:26 -0400

    Move sale properties from Solidity to JS

--- contracts/tokensale/MultiRoundCrowdsale.sol
+++ contracts/tokensale/MultiRoundCrowdsale.sol
@@ -1,9 +1,10 @@
 pragma solidity ^0.4.23;
 
 import "./Crowdsale.sol";
+import "../lib/Ownable.sol";
 import "../math/SafeMath.sol";
 
-contract MultiRoundCrowdsale is Crowdsale {
+contract MultiRoundCrowdsale is  Crowdsale, Ownable {
 
     using SafeMath for uint256;
 
@@ -13,27 +14,6 @@ contract MultiRoundCrowdsale is Crowdsale {
         uint256 rate;
     }
 
-    uint256 constant _baseRate = 10000;
-    uint256 constant _seedRoundRate = 30000;
-    uint256 constant _presaleRate = 15000;
-    uint256 constant _crowdsaleWeek1Rate = 13000;
-    uint256 constant _crowdsaleWeek2Rate = 12000;
-    uint256 constant _crowdsaleWeek3Rate = 11000;
-    uint256 constant _crowdsaleWeek4Rate = 10000;
-
-    uint256 constant _seedRoundStart = 1527811200;
-    uint256 constant _seedRoundEnd = 1529020800;
-    uint256 constant _presaleStart = 1531440000;
-    uint256 constant _presaleEnd = 1532044800;
-    uint256 constant _crowdsaleWeek1Start = 1532044800;
-    uint256 constant _crowdsaleWeek1End = 1532044800;
-    uint256 constant _crowdsaleWeek2Start = 1532044800;
-    uint256 constant _crowdsaleWeek2End = 1532044800;
-    uint256 constant _crowdsaleWeek3Start = 1532044800;
-    uint256 constant _crowdsaleWeek3End = 1532044800;
-    uint256 constant _crowdsaleWeek4Start = 1532044800;
-    uint256 constant _crowdsaleWeek4End = 1532044800;
-
     SaleRound seedRound;
     SaleRound presale;
     SaleRound crowdsaleWeek1;
@@ -41,16 +21,41 @@ contract MultiRoundCrowdsale is Crowdsale {
     SaleRound crowdsaleWeek3;
     SaleRound crowdsaleWeek4;
 
-    constructor() {
-        seedRound      = SaleRound(_seedRoundStart, _seedRoundEnd, _seedRoundRate);
-        presale        = SaleRound(_presaleStart, _presaleEnd, _presaleRate);
-        crowdsaleWeek1 = SaleRound(_crowdsaleWeek1Start, _crowdsaleWeek1End, _crowdsaleWeek1Rate);
-        crowdsaleWeek2 = SaleRound(_crowdsaleWeek2Start, _crowdsaleWeek2End, _crowdsaleWeek2Rate);
-        crowdsaleWeek3 = SaleRound(_crowdsaleWeek3Start, _crowdsaleWeek3End, _crowdsaleWeek3Rate);
-        crowdsaleWeek4 = SaleRound(_crowdsaleWeek4Start, _crowdsaleWeek4End, _crowdsaleWeek4Rate);
+    bool public saleRoundsSet = false;
+
+    function setTokenSaleRounds(uint256[3] _seedRound, uint256[3] _presale, uint256[3] _crowdsaleWeek1, uint256[3] _crowdsaleWeek2, uint256[3] _crowdsaleWeek3, uint256[3] _crowdsaleWeek4) external onlyOwner returns (bool) {
+        // This function can only be called once
+        require(!saleRoundsSet);
+
+        // Check that each round end time is after the start time
+        require(_seedRound[0] < _seedRound[1]);
+        require(_presale[0] < _presale[1]);
+        require(_crowdsaleWeek1[0] < _crowdsaleWeek1[1]);
+        require(_crowdsaleWeek2[0] < _crowdsaleWeek2[1]);
+        require(_crowdsaleWeek3[0] < _crowdsaleWeek3[1]);
+        require(_crowdsaleWeek4[0] < _crowdsaleWeek4[1]);
+
+        // Check that each round ends before the next begins
+        require(_seedRound[1] < _presale[0]);
+        require(_presale[1] < _crowdsaleWeek1[0]);
+        require(_crowdsaleWeek1[1] < _crowdsaleWeek2[0]);
+        require(_crowdsaleWeek2[1] < _crowdsaleWeek3[0]);
+        require(_crowdsaleWeek3[1] < _crowdsaleWeek4[0]);
+
+        seedRound      = SaleRound(_seedRound[0], _seedRound[1], _seedRound[2]);
+        presale        = SaleRound(_presale[0], _presale[1], _presale[2]);
+        crowdsaleWeek1 = SaleRound(_crowdsaleWeek1[0], _crowdsaleWeek1[1], _crowdsaleWeek1[2]);
+        crowdsaleWeek2 = SaleRound(_crowdsaleWeek2[0], _crowdsaleWeek2[1], _crowdsaleWeek2[2]);
+        crowdsaleWeek3 = SaleRound(_crowdsaleWeek3[0], _crowdsaleWeek3[1], _crowdsaleWeek3[2]);
+        crowdsaleWeek4 = SaleRound(_crowdsaleWeek4[0], _crowdsaleWeek4[1], _crowdsaleWeek4[2]);
+
+        saleRoundsSet = true;
+        return saleRoundsSet;
     }
 
     function getCurrentRate() public view returns (uint256) {
+        require(saleRoundsSet);
+
         uint256 currentTime = block.timestamp;
         if (currentTime > seedRound.start && currentTime <= seedRound.end) {
             return seedRound.rate;
@@ -70,15 +75,12 @@ contract MultiRoundCrowdsale is Crowdsale {
     }
 
     function getTokenAmount(uint256 _weiAmount) internal view returns (uint256) {
+        require(_weiAmount != 0);
+
         uint256 currentRate = getCurrentRate();
+
+        require(currentRate != 0);
+        
         return currentRate.mul(_weiAmount);
-    }
-
-    function secondsInDay() internal view returns (uint256) {
-        return 60 * 60 * 24;
-    }
-
-    function secondsInWeek() internal view returns (uint256) {
-        return secondsInDay() * 7;
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,39 +3,36 @@ var TipTokenCrowdsale = artifacts.require("./TipTokenCrowdsale")
 
 var moment = require('moment')
 
-const seedRoundStart    = moment('2018-06-01T00:00:00Z').unix()
-const seedRoundEnd      = moment('2018-06-15T23:59:59Z').unix()
-const presaleStart      = moment('2018-07-13T00:00:00Z').unix()
-const presaleEnd        = moment('2018-07-20T23:59:59Z').unix()
-const crowdsaleStart    = moment('2018-07-21T00:00:00Z').unix()
-const crowdsaleEnd      = moment('2018-08-17T23:59:59Z').unix()
+const seedRoundStart        = moment('2018-06-01T00:00:00Z').unix()
+const seedRoundEnd          = moment('2018-06-15T23:59:59Z').unix()
+const presaleStart          = moment('2018-07-13T00:00:00Z').unix()
+const presaleEnd            = moment('2018-07-20T23:59:59Z').unix()
 
-const crowdsaleWeek1Start   = moment('2018-07-13T00:00:00Z').unix()
-const crowdsaleWeek1End     = moment('2018-07-13T00:00:00Z').unix()
-const crowdsaleWeek2Start   = moment('2018-07-13T00:00:00Z').unix()
-const crowdsaleWeek2End     = moment('2018-07-13T00:00:00Z').unix()
-const crowdsaleWeek3Start   = moment('2018-07-13T00:00:00Z').unix()
-const crowdsaleWeek3End     = moment('2018-07-13T00:00:00Z').unix()
-const crowdsaleWeek4Start   = moment('2018-07-13T00:00:00Z').unix()
-const crowdsaleWeek4End     = moment('2018-07-13T00:00:00Z').unix()
+const crowdsaleWeek1Start   = moment('2018-07-21T00:00:00Z').unix()
+const crowdsaleWeek1End     = moment('2018-07-27T23:59:59Z').unix()
+const crowdsaleWeek2Start   = moment('2018-07-28T00:00:00Z').unix()
+const crowdsaleWeek2End     = moment('2018-08-03T23:59:59Z').unix()
+const crowdsaleWeek3Start   = moment('2018-08-04T00:00:00Z').unix()
+const crowdsaleWeek3End     = moment('2018-08-10T23:59:59Z').unix()
+const crowdsaleWeek4Start   = moment('2018-08-11T00:00:00Z').unix()
+const crowdsaleWeek4End     = moment('2018-08-17T23:59:59Z').unix()
 
-const testSaleStart     = moment().add(1, 'minute')
-const testSaleEnd       = moment().add(6, 'minute')
-const testSaleStartUnix = testSaleStart.unix()
-const testSaleEndUnix   = testSaleEnd.unix()
+const testSaleStart         = moment().add(1, 'minute')
+const testSaleEnd           = moment().add(6, 'minute')
+const testSaleStartUnix     = testSaleStart.unix()
+const testSaleEndUnix       = testSaleEnd.unix()
 
 const baseRate              = 10000
-const presaleRate           = baseRate * 1.5
 const seedRoundRate         = baseRate * 3.0
+const presaleRate           = baseRate * 1.5
 const crowdsaleWeek1Rate    = baseRate * 1.3
 const crowdsaleWeek2Rate    = baseRate * 1.2
 const crowdsaleWeek3Rate    = baseRate * 1.1
 const crowdsaleWeek4Rate    = baseRate
 
+const hardCap               = web3.toWei(50000, "ether")
 
-const hardCap           = web3.toWei(50000, "ether")
-
-const timeFormat        = "MMMM DD YYYY, h:mm:ss a"
+const timeFormat            = "MMMM DD YYYY, h:mm:ss a"
 console.log("Sale start time = ", testSaleStart.format(timeFormat));
 console.log("Sale end time = ", testSaleEnd.format(timeFormat));
 
@@ -43,39 +40,16 @@ module.exports = (deployer, network, accounts) => {
     return deploy(deployer, accounts)
 }
 
-async function liveDeploy(deployer, accounts) {
-
-    let seedRound = [seedRoundStart, seedRoundEnd, seedRoundRate]
-    let presale = [presaleStart, presaleEnd,presaleRate]
-    let crowdsaleWeek1 = [crowdsaleWeek1Start, crowdsaleWeek1End, crowdsaleWeek1Rate]
-    let crowdsaleWeek2 = [crowdsaleWeek2Start, crowdsaleWeek2End, crowdsaleWeek2Rate]
-    let crowdsaleWeek3 = [crowdsaleWeek3Start, crowdsaleWeek3End, crowdsaleWeek3Rate]
-    let crowdsaleWeek4 = [crowdsaleWeek4Start, crowdsaleWeek4End, crowdsaleWeek4Rate]
-    let wallet = accounts[0]
-    let vault = accounts[9]
-
-        return deployer.deploy(
-            TipTokenCrowdsale,
-            token, vault, wallet, hardCap,
-            seedRound, presale, crowdsaleWeek1, crowdsaleWeek2, crowdsaleWeek3, crowdsaleWeek4,
-        )
-        .then(async (instance) => {
-           const crowdsale = await TipTokenCrowdsale.deployed()
-           const token = await crowdsale.token.call()
-           console.log("Tip Token deployed at address: ", token);
-        })
-}
-
 async function deploy(deployer, accounts) {
-    let tokenWallet = accounts[0]
-    let ethVault = accounts[1]
+    let tokenWallet     = accounts[0]
+    let ethVault        = accounts[1]
 
-    let seedRound = [seedRoundStart, seedRoundEnd, seedRoundRate]
-    let presale = [presaleStart, presaleEnd,presaleRate]
-    let crowdsaleWeek1 = [crowdsaleWeek1Start, crowdsaleWeek1End, crowdsaleWeek1Rate]
-    let crowdsaleWeek2 = [crowdsaleWeek2Start, crowdsaleWeek2End, crowdsaleWeek2Rate]
-    let crowdsaleWeek3 = [crowdsaleWeek3Start, crowdsaleWeek3End, crowdsaleWeek3Rate]
-    let crowdsaleWeek4 = [crowdsaleWeek4Start, crowdsaleWeek4End, crowdsaleWeek4Rate]
+    let seedRound       = [seedRoundStart, seedRoundEnd, seedRoundRate]
+    let presale         = [presaleStart, presaleEnd,presaleRate]
+    let crowdsaleWeek1  = [crowdsaleWeek1Start, crowdsaleWeek1End, crowdsaleWeek1Rate]
+    let crowdsaleWeek2  = [crowdsaleWeek2Start, crowdsaleWeek2End, crowdsaleWeek2Rate]
+    let crowdsaleWeek3  = [crowdsaleWeek3Start, crowdsaleWeek3End, crowdsaleWeek3Rate]
+    let crowdsaleWeek4  = [crowdsaleWeek4Start, crowdsaleWeek4End, crowdsaleWeek4Rate]
 
     return deployer
     .then(() => {
@@ -88,6 +62,12 @@ async function deploy(deployer, accounts) {
             tokenWallet, ethVault, hardCap,
             seedRoundStart, crowdsaleWeek4End, baseRate
         )
+    })
+    .then(() => {
+        return TipTokenCrowdsale.deployed()
+    })
+    .then(crowdsaleInstance => {
+        crowdsaleInstance.setTokenSaleRounds(seedRound, presale, crowdsaleWeek1, crowdsaleWeek2, crowdsaleWeek3, crowdsaleWeek4)
     })
     .catch(err => {
         console.log("Error deploying contracts: ", err)
