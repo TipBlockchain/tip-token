70ec39882d5a60b7520890d61973f0a0d0abad3b
John Warmann <john.warmann@gmail.com>
2018-05-09 13:15:58 -0400

    Turned on optimization

--- truffle.js
+++ truffle.js
@@ -1,30 +1,34 @@
 module.exports = {
      // See <http://truffleframework.com/docs/advanced/configuration>
      // to customize your Truffle configuration!
+     solc: {
+         optimizer: { enabled: true, runs: 200 }
+     },
      networks: {
           development: {
                host: "localhost",
                port: 7545,
                network_id: "*", // Match any network id
-               gas: 5100000
+               gas: 4700000
           },
           private: {
             host: "localhost",
             port: 8545,
             network_id: "4224",
-            gas: 5100000
+            gas: 4700000
           },
           rinkeby: {
             host: "localhost",
             port: 8545,
             network_id: "4", //rinkeby test network
-            gas: 5100000
+            gas: 4700000,
+            gasPrice: 100000000000
           },
           live: {
             host: "localhost",
             port: 8545,
             network_id: "1",
-            gas: 5100000,
+            gas: 4700000,
             gasPrice: 57000000000,
             from: "0xbf6f5e78e154799c7856f4cf9f269cf7a7201310"
           }
